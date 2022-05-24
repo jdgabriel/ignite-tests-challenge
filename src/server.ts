ac389f43b51f6431b38310fd5e282cfe5bce8406
jdgabriel <jds.gabrielduarte@gmail.com>
2022-05-24 08:57:04 -0300

    log running on port

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
-import { app } from './app';
-
-app.listen(3333, () => { console.log('Server is running') });
+import { app } from "./app";
+app.listen(3333, () => {
+  console.log("Server is running at http://localhost:3333/");
+});
